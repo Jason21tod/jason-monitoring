5072c78a231ee9b981fc3a3043cafccaaeb39dcd
Gian_Pereira <gian.21.otaku@gmail.com>
2026-04-20 13:25:17 -0300

    Started the first setup for server

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
+from twilio.rest import Client
+import os
 
+
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SSID")
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
+
+
+twilio_client: Client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 app = FastAPI()
 
