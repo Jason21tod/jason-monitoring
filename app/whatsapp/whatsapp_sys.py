abf9c5dc205d44c3da9d77bd2f1050c4789a3362
Gian_Pereira <gian.21.otaku@gmail.com>
2026-09-05 08:16:10 -0300

    refactored the message send system

--- app/whatsapp/whatsapp_sys.py
+++ app/whatsapp/whatsapp_sys.py
@@ -9,6 +9,36 @@ TESTER_CELLPHONE_NUMBER = os.environ.get("TESTER_CELLPHONE_NUMBER")
 
 
 
+class MessageSender:
+    def __init__(self, msg_object: MsgObject) -> None:
+        self.msg_object: MsgObject = msg_object    
+
+    def is_necessary_multiple_responses(self, current_char: str, char_count: int):
+        if current_char == "\n":
+            if self.is_in_char_limit(char_count):
+                return True
+        return False
+
+    def is_in_char_limit(self, char_count: int):
+        if char_count >= 1500:
+            return True
+        return False
+
+    def send_char_limit_messages(self, char_count:int):
+        first_body =  self.msg_object.body[0:char_count]
+        self.create_response(first_body)
+
+        second_body = self.msg_object.body[char_count:]
+        self.create_response(second_body)
+
+    def create_response(self, body):
+        print(f"response -> {body}")
+        print(twilio_client.messages.create(
+                    from_=self.msg_object.to,
+                    body= body,
+                    to= self.msg_object._from
+                ).status)
+
 logging.basicConfig(
     level=logging.INFO,
     format=" %(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -34,33 +64,23 @@ def send_test_message(name: str, customer_number: str):
     return "400"
 
 def send_message(msg_object: MsgObject):
-    print(msg_object.body)
-    line = 0
-    char_count = 0
-    for char in msg_object.body:
-        char_count += 1
-        if char == "\n":
-            line += 1
-            if char_count >= 1500:
-                twilio_client.messages.create(
-                                from_=msg_object.to,
-                                body= msg_object.body[0:char_count],
-                                to= msg_object._from
-                            )
-                twilio_client.messages.create(
-                                from_=msg_object.to,
-                                body= msg_object.body[char_count:],
-                                to= msg_object._from
-                            )
-                return 200
-                         
     if msg_object._from != "None":
-        message = twilio_client.messages.create(
-                from_=msg_object.to,
-                body= msg_object.body,
-                to= msg_object._from
-            )
-        whatsapp_logger.info(message.status)
-        return 200
+        return send_messages(msg_object)
     whatsapp_logger.warning("ERROR -> COULD NOT SEND MESSAGE! THE TARGET IS NONE")
     return 400
+
+def send_messages(msg_object: MsgObject):
+    message_sender = MessageSender(msg_object)
+    verify_multiple_messages(message_sender)
+    message_sender.create_response(msg_object.body)
+    return 200
+
+def verify_multiple_messages(message_sender: MessageSender):
+    char_count = 0
+    for current_char in message_sender.msg_object.body:
+        char_count += 1
+        if message_sender.is_necessary_multiple_responses(current_char, char_count):
+            message_sender.send_char_limit_messages(char_count)
+            return True
+    else:
+        return False
